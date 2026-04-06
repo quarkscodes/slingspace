2a184dbf203dac95d33dce1c77b07a841e1d2df9
Gix <schisleradam@gmail.com>
2026-04-06 12:49:38 -0400

    updated interpolation weight on camera to be snappier

--- player/camera_pivot.gd
+++ player/camera_pivot.gd
@@ -5,7 +5,7 @@ extends Node3D
 # I think the issue lies in the setup of the target_basis but I could be wrong
 func _process(delta: float) -> void:
 	var target_xform: Transform3D = player.get_global_transform_interpolated()
-	basis = basis.slerp(target_xform.basis, 1.5 * delta)
+	basis = basis.slerp(target_xform.basis, 3.0 * delta)
 	basis = basis.orthonormalized()
 	
 	position = player.position
