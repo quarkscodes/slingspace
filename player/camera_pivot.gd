acc1607b2f7b666bf3f8e57597050f576fd18304
Gix <schisleradam@gmail.com>
2026-04-06 12:56:59 -0400

    added camera pitch up and down controls and mapped to right joystick vertical axis

--- player/camera_pivot.gd
+++ player/camera_pivot.gd
@@ -8,4 +8,7 @@ func _process(delta: float) -> void:
 	basis = basis.slerp(target_xform.basis, 3.0 * delta)
 	basis = basis.orthonormalized()
 	
+	var camera_angular_velocity: float = Input.get_axis("camera_pitch_up", "camera_pitch_down") * 0.5
+	basis = basis.rotated(basis.x, -camera_angular_velocity * PI * delta)
+	
 	position = player.position
