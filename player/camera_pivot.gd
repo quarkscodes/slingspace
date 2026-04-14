8e9a7332df373a1f404e4a605a83074e20ba803d
Gix <schisleradam@gmail.com>
2026-04-14 14:50:41 -0400

    finished tutorial part one to create a mesh cube and turn into normalized sphere

--- player/camera_pivot.gd
+++ player/camera_pivot.gd
@@ -1,9 +1,9 @@
 extends Node3D
 
-@export var input_response = 0.6
-@export var interpolation_weight = 3.0
+@export var input_response := 0.6
+@export var interpolation_weight := 3.0
 
-@onready var player: RigidBody3D = %Player
+@onready var player := %Player
 
 
 func _process(delta: float) -> void:
@@ -11,7 +11,7 @@ func _process(delta: float) -> void:
 	basis = basis.slerp(target_xform.basis, interpolation_weight * delta)
 	basis = basis.orthonormalized()
 	
-	var camera_angular_velocity: float = \
+	var camera_angular_velocity := \
 		Input.get_axis("camera_pitch_up", "camera_pitch_down") * input_response
 	basis = basis.rotated(basis.x, -camera_angular_velocity * PI * delta)
 	
